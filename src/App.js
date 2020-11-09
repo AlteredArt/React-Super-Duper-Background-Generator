9f8c6566c4cf65a510c461d23ab64e67923dc468
Altered Art <cherokeedreemer@gmail.com>
2020-11-09 12:27:23 -0700

    adding home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      hello
+      <h1>Generate your gradient background!</h1>
+      <h2>Then Copy and Paste!</h2>
+      <hr />
+      <input class="color1" type="color" name="color1" value="#FFFFFF" />
+      <input class="color2" type="color" name="color2" value="#FFFFFF" />
+      <hr />
+      <button id='radial'>Radial</button>
+      <button id='linear'>Linear</button><br />
     </div>
   );
 }
