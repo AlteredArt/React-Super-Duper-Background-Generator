a00c70c7d06f7ff6c662395bc1d33558847bbbba
Altered Art <cherokeedreemer@gmail.com>
2020-11-10 17:27:47 -0700

    fix read me

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Switch, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import Home from './Components/Home'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <Switch>
+            <Route exact path='/' component={App} />
+            <Router path='/home' component={Home} />
+        </Switch>
+    </Router>,
+    document.getElementById('root'));
 
 
